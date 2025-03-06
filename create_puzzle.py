24a744aec5d92974d8cbd58122f5c8d6f56680bb
alfadefender <serejenk4.m@yandex.ru>
2025-03-06 17:49:48 +0500

    added marking function, function to find way between two points

--- create_puzzle.py
+++ create_puzzle.py
@@ -1,31 +1,94 @@
+from queue import Queue
 import random
+from copy import deepcopy
 
-def debug_pretty_print(graph):
+
+def pretty_print(graph: list[list]) -> None:
     for line in graph:
         print(*line)
 
-def debug_checkup_bruteforce(graph):
-    pass
+def find_directions(n: int, cur_pos: tuple) -> list[tuple]:
+    directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
-def create_random(n: int, m: int = None):
-    if m is None:
-        m = n
+    if cur_pos[0] == 0:  # строка idx=0 => вверх нельзя
+        directions[0] = 0
+    if cur_pos[1] == n - 1:  # столбец idx=n-1 => вправо нельзя
+        directions[1] = 0
+    if cur_pos[0] == n - 1:  # строка idx=n-1 => вниз нельзя
+        directions[2] = 0
+    if cur_pos[1] == 0:  # столбец idx=0 => влево нельзя
+        directions[3] = 0
 
-    graph = [[0] * n for i in range(n)]
-    pool = [i for i in range(1, n*n + 1)]
+    return directions
 
-    for idx in range(1, m + 1):
-        for j in range(2):
-            pos = random.choice(pool)
-            pool.remove(pos)
+# point = tuple(y, x)
+def marking(graph: list[list], n: int, point1: tuple) -> list[list]:
+    que = Queue()
+    que.put((point1,))
+    cur_mark = -1
 
-            graph[pos//n - 1][pos%n - 1] = idx
+    while not que.empty():
+        positions = que.get()
+        new_positions = []
+        for cur_pos in positions:
+            # up | right | down | left
+            directions = find_directions(n, cur_pos)
 
-    debug_pretty_print(graph)
+            for direction in directions:
+                if direction:
+                    next_pos = (direction[0] + cur_pos[0], direction[1] + cur_pos[1])
+                    if graph[next_pos[0]][next_pos[1]] == 0:
+                        graph[next_pos[0]][next_pos[1]] = cur_mark
+                        new_positions += [next_pos]
+
+        if new_positions:
+            que.put(tuple(new_positions))
+        cur_mark -= 1
+
     return graph
 
+def find_way(graph: list[list], n: int, point1: tuple, point2: tuple) -> list[list]:
+    cur_graph = marking(deepcopy(graph), n, point1)
+    all_ways = []
+    pretty_print(cur_graph)
+
+    def wrapper_find_way(cur_pos: tuple, used: list, way: list):
+        nonlocal all_ways
+        directions = find_directions(n, cur_pos)
+        is_end = False
+
+        for direction in directions:
+            if direction:
+                next_pos = (
+                direction[0] + cur_pos[0], direction[1] + cur_pos[1])
+                if next_pos == point2:
+                    is_end = True
+                if cur_graph[next_pos[0]][next_pos[1]] < 0 and used.count(next_pos) == 0:
+                    wrapper_find_way(next_pos, used + [next_pos], way + [next_pos])
+
+        if is_end:
+            way += [point2]
+            all_ways += [way]
+
+    wrapper_find_way(point1, [point1], [point1])
+
+    return all_ways
+
+def checkup_bruteforce(graph: list[list]) -> bool:
+    pass
+
 if __name__ == "__main__":
-    create_random(5, 5)
+    graph = [[2, 0, 1, 6, 5, 0],
+             [1, 0, 0, 0, 0, 0],
+             [0, 0, 0, 4, 6, 0],
+             [0, 2, 0, 0, 0, 0],
+             [0, 0, 0, 3, 0, 5],
+             [3, 0, 0, 0, 0, 4]]
+
+    all_ways = find_way(graph, 6, (2, 3), (5, 5))
+    print(len(all_ways))
+    for way in all_ways:
+        print(way, len(way))
 
 """
 test true
